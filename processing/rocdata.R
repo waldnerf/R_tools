233fa2497fd65e9f0465a5e56841e8a683ee88ff
Francois Waldner <francois.waldner@gmail.com>
2017-07-15 10:33:02 +0200

    First commit

---
+++ processing/rocdata.R
@@ -0,0 +1,55 @@
+rocdata <- function(grp, pred){
+  #' Produces x and y co-ordinates for ROC curve plot
+  #' 
+  #' This function produces x and y co-ordinates for ROC curve plot
+  #' 
+  #' @param grp labels classifying subject status
+  #' @param pred values of each observation
+  #' @param ref is a vector of labels as provided by the validation data 
+  #' @return List with 2 components:
+  #'         roc = data.frame with x and y co-ordinates of plot
+  #'         stats = data.frame containing: area under ROC curve, p value, upper and lower 95% confidence interval
+
+  grp <- as.factor(grp)
+  if (length(pred) != length(grp)) {
+    stop("The number of classifiers must match the number of data points")
+  } 
+  
+  if (length(levels(grp)) != 2) {
+    stop("There must only be 2 values for the classifier")
+  }
+  
+  #cut <- sort(unique(pred))
+  cut <- (c(-Inf, sort(unique(pred))) + c(sort(unique(pred)), +Inf))/2 # pROC thresholds
+  tp <- sapply(cut, function(x) length(which(pred > x & grp == levels(grp)[2])))
+  fn <- sapply(cut, function(x) length(which(pred < x & grp == levels(grp)[2])))
+  fp <- sapply(cut, function(x) length(which(pred > x & grp == levels(grp)[1])))
+  tn <- sapply(cut, function(x) length(which(pred < x & grp == levels(grp)[1])))
+  tpr <- tp / (tp + fn)
+  fpr <- fp / (fp + tn)
+  roc = data.frame(x = fpr, y = tpr)
+  roc <- roc[order(roc$x, roc$y),]
+  
+  i <- 2:nrow(roc)
+  auc <- (roc$x[i] - roc$x[i - 1]) %*% (roc$y[i] + roc$y[i - 1])/2
+  
+  pos <- pred[grp == levels(grp)[2]]
+  neg <- pred[grp == levels(grp)[1]]
+  q1 <- auc/(2-auc)
+  q2 <- (2*auc^2)/(1+auc)
+  se.auc <- sqrt(((auc * (1 - auc)) + ((length(pos) -1)*(q1 - auc^2)) + ((length(neg) -1)*(q2 - auc^2)))/(length(pos)*length(neg)))
+  ci.upper <- auc + (se.auc * 0.96)
+  ci.lower <- auc - (se.auc * 0.96)
+  
+  se.auc.null <- sqrt((1 + length(pos) + length(neg))/(12*length(pos)*length(neg)))
+  z <- (auc - 0.5)/se.auc.null
+  p <- 2*pnorm(-abs(z))
+  
+  stats <- data.frame (auc = auc,
+                       p.value = p,
+                       ci.upper = ci.upper,
+                       ci.lower = ci.lower
+  )
+  
+  return (list(roc = roc, stats = stats))
+}
